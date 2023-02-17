011e91d2284c4bd44a04440e181366eb49bb366c
Lukkelele <encryptedluke@protonmail.com>
2023-02-17 18:39:09 +0100

    STILL TRYING to fix the cmake build

--- VMS/include/Log.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#define LOG(x) printf("%s\n", x)
--- compile.sh
+++ compile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# This needs to be run at ROOT
-
 ./clean.sh
 # cmake --debug-output -B ./build -S .
-cmake -DCMAKE_BUILD_TYPE=Debug -B ./build -S .
+#cmake -DCMAKE_BUILD_TYPE=Debug -B ./build -S .
+cmake --debug-output -B ./build -S .
 make -C build
