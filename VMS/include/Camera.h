d39efda01d3ca77b967272c69d26f158b75cd68d
Lukkelele <encryptedluke@protonmail.com>
2023-02-17 10:43:54 +0100

    Added fix for the compiling error for mutex

--- VMS/include/Camera.h
+++ VMS/include/Camera.h
@@ -1,20 +1,19 @@
+#include <stdio.h>
 #include <functional>
 #include <memory>
 #include <string>
-#include <mutex>
 
-#include <opencv2/core.hpp>
-#include <opencv2/core/mat.hpp>
-#include <opencv2/core/utility.hpp>
 
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
 
 #include "Log.h"
 
 namespace VMS {
+
 
     class Camera
     {
--- VMS/src/Camera.cpp
+++ VMS/src/Camera.cpp
@@ -7,11 +7,11 @@ namespace VMS {
     {
         // m_Capture = std::make_shared<cv::VideoCapture>(0);
         m_Capture = cv::VideoCapture(0);
-        if (!m_Capture.isOpened())
-        {
-            LOG("[ERROR] Couldn't create camera");
-        }
-        std::string imagePath = "../../img/template.png";
+        //if (!m_Capture.isOpened())
+        //{
+        //    LOG("[ERROR] Couldn't create camera");
+        //}
+        // std::string imagePath = "../../img/template.png";
         // cv::Mat m_Template = cv::imread(imagePath);
     }
 
