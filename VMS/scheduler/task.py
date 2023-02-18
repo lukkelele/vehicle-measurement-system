adbad8bb6a6b2e0a4aa0b68f7dee52056f3ca600
Lukkelele <encryptedluke@protonmail.com>
2023-02-18 11:49:29 +0100

    Micropython test program + camera processor/camera class

--- VMS/scheduler/task.py
+++ VMS/scheduler/task.py
@@ -5,13 +5,12 @@ from micropython import const
 from utime import sleep_ms
 
 class Task:
-    def __init__(self, name, delay_ms, priority):
-        # Save the task name, delay, and priority
+    def __init__(self, name, delay_ms=500, priority=1):
         self.name = name
         self.delay_ms = delay_ms
         self.priority = priority
         
-        # Start the task in a separate thread
+        # Start task
         _thread.start_new_thread(self.run, ())
 
     def run(self):
@@ -23,26 +22,5 @@ class Task:
             sleep_ms(self.delay_ms)
             
     def task(self):
-        # Override this method to implement your task logic
+        # LOGIC HERE
         pass
-
-# Define constants
-TASK1_NAME = const(1)
-TASK1_DELAY_MS = const(1000)
-TASK1_PRIORITY = const(1)
-
-TASK2_NAME = const(2)
-TASK2_DELAY_MS = const(2000)
-TASK2_PRIORITY = const(2)
-
-# Define two tasks that inherit from the Task class
-class Task1(Task):
-    def task(self):
-        print("Task 1 running")
-
-class Task2(Task):
-    def task(self):
-        print("Task 2 running")
-
-# Start the FreeRTOS scheduler to run the tasks
-micropython.schedule(schedule)
